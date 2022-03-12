45fdd05df32a663563256afa80832e893f14b8bf
Programming Hero <programming.hero1@gmail.com>
2022-03-11 19:01:40 -0600

    set initial navbar

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <h2 className='text-4xl'>Save most of your money</h2>
+            <p>By giving your money to me!!!</p>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { MenuIcon, XIcon } from '@heroicons/react/solid';
+
+const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    const routes = [
+        {id: 1, name: 'Home', link: '/home'},
+        {id: 2, name: 'Shop', link: '/shop'},
+        {id: 3, name: 'Deals', link: '/deals'},
+        {id: 4, name: 'Coupons', link: '/coupons'},
+        {id: 4, name: 'Contact', link: '/contact'},
+    ]
+    return (
+        <nav>
+            <div onClick={() => setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className={`md:flex justify-center absolute duration-500 ease-in ${open ? 'top-6': 'top-[-120px]'}`}>
+                {
+                    routes.map(route => <Link 
+                        key={route.id}
+                        route={route}
+                    ></Link>)
+                }
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
